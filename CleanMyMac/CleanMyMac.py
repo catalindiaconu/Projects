9941905349e319e9a72854a0160390d8a7ed1401
Gelu Catalin Diaconu <catalincsro@gmail.com>
2020-08-18 22:02:49 +0300

    update CleanMyMac

--- CleanMyMac/CleanMyMac.py
+++ CleanMyMac/CleanMyMac.py
@@ -2,14 +2,13 @@
 import os
 import getpass
 
-# passwd = getpass.getpass()
+passwd = getpass.getpass()
 
 
 def delete_caches():
     print("\nLocation: ~/Library/Caches")
     caches_location = os.path.expanduser("~/Library/Caches/")  # used to allow the use of tilde (~) sign
     caches_objects = os.listdir(caches_location)
-    f = open("log.txt", "a")
     for i in caches_objects:
         os.system("echo {} | sudo -S rm -rf {}{}".format(passwd, caches_location, i))
         print("deleting: {}".format(i))
@@ -27,11 +26,14 @@ def delete_user_logs():
 def delete_languages():
     languages_location = "/System/Applications/"
     languages_objects = os.listdir(languages_location)
-    for i in languages_objects:
-        os.system("du -hs {}{}/Contents/Resources".format(languages_location, i))
-        # os.system("ls -al {}{}/Contents/Resources/*.lproj".format(languages_location, i))
+    os.system("du -hs {}*.app/Contents/Resources".format(languages_location))
+    # os.system("find {}*.app/Contents/Resources -name '*.lproj' -type d -not -path '{}*.app/Contents/Resources/en*' -exec rm -rf {} \;".format(languages_location, languages_location))
 
 
-# delete_caches()
-# delete_user_logs()
-delete_languages()
+delete_caches()
+delete_user_logs()
+# delete_languages()
+# Errors reported:
+# - Operation not permitted (even with SUDO): SIP must be disabled.
+# - Read-only file system: even with SIP disabled. Because the files are stored on /
+# Plus the space taken by the files is not that big.
